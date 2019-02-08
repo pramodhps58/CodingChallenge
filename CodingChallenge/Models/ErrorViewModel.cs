75eb734f031a3ed59d927a64ec46d3d8d3c802ba
Sai Parimisetty <pramodhps5845@hotmail.com>
2019-02-08 00:07:17 -0600

    Added Project to solution

---
+++ CodingChallenge/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodingChallenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
